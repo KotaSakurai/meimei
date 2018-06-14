dff66ffcaa0ec8ee0185b2df5250875631238276
KotaSakurai <sakurai.kota@mwed.co.jp>
2018-06-14 21:41:05 +0900

    controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -6,8 +6,20 @@ class WordsController < ApplicationController
   end
 
   def new
+    @word = Word.new
   end
 
   def create
+    @word = Word.new(word_params)
+
+    if @word.save
+      redirect_to word_path
+    end
   end
+
+  private
+
+    def word_params
+        params.require(:word).permit(:word)
+    end
 end
