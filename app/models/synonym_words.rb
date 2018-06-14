ed4ffce5c9715692e6b288a8e274811be08b49f7
kiyokuro <kuro6412@gmail.com>
2018-06-14 20:53:28 +0900

    add models

--- app/models/synonym_words.rb
+++
@@ -1,4 +0,0 @@
-class SynonymWords < ApplicationRecord
-  belongs_to :original, class_name: "Words"
-  belongs_to :similar, class_name: "Words"
-end
--- app/models/words.rb
+++
@@ -1,4 +0,0 @@
-class Words < ApplicationRecord
-  has_many :synonym_words, class_name: "SynonymWords", foreign_key: "similar_id", dependent: :destroy
-  has_many :original, through: :synonym_words, source: :similar
-end
